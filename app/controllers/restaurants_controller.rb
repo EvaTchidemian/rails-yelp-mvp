46de6bea16226e1f697bbd03b628b7b98ddb8c98
Eva Tchidemian <e.tchidemian@yahoo.fr>
2023-02-22 16:50:16 +0100

    crud

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,37 @@
+class RestaurantsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(restaurant_params)
+    @review = Review.new
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'french' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'french' }
+tao_bento = { name: 'TaoBento', address: 'Marcq', phone_number: '0320000001', category: 'japanese' }
+bellezza = { name: 'Bellezza', address: 'Lille', phone_number: '0320000002', category: 'italian' }
+so_good = { name: 'SoGood', address: 'Bondues', phone_number: '0320000002', category: 'french' }
+
+[dishoom, pizza_east, tao_bento, bellezza, so_good].each do |attributes|
+  Restaurant.create!(attributes)
+end
